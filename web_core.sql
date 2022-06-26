51b6ab2e7ccb7f6f534a3b356cfd79dde3159c87
Expolite <macompas.ronnel@gmail.com>
2022-06-26 22:13:59 +0800

    update sql

--- web_core.sql
+++ web_core.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 26, 2022 at 01:26 PM
+-- Generation Time: Jun 26, 2022 at 04:12 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -62,7 +62,9 @@ INSERT INTO `activity_log` (`activity_log_id`, `user_id`, `date_log`, `action`, 
 (18, 1, '2022-06-13 10:29:27', 'Reset Password Admin Account:nel - DETAILS : \r\n({\"ADMIN_ID\":\"8\",\"ADMIN_NO\":\"nel\",\"FIRSTNAME\":\"neru\",\"LASTNAME\":\"tokisaki\",\"USERNAME\":\"nel\",\"USER_ROLE\":\"2\"})', '4d0896c27845ebc40376ef3c310cd08997bba1ecc0870dc60489a38dda14e6ed', '1', 0),
 (19, 1, '2022-06-13 11:46:39', 'Update User nel USER_ID:8 - FROM Details {\"USER_ROLE\":\"2\"} TO {\"USER_ROLE\":\"3\"}', '4a882e54b6c2ebf34dec6872faedf296f7b08b5fbd41b05b14550afcfb6a42af', '1', 0),
 (20, 1, '2022-06-13 11:47:18', 'Update User nel USER_ID:8 - FROM Details {\"USER_ROLE\":\"3\"} TO {\"USER_ROLE\":\"2\"}', '4a882e54b6c2ebf34dec6872faedf296f7b08b5fbd41b05b14550afcfb6a42af', '1', 0),
-(21, 1, '2022-06-21 10:26:56', 'Reset Password Admin Account:ituser - DETAILS : \r\n({\"ADMIN_ID\":\"1\",\"ADMIN_NO\":\"ituser\",\"FIRSTNAME\":\"aaaa\",\"LASTNAME\":\"ddd\",\"USERNAME\":\"ituser\",\"USER_ROLE\":\"1\"})', 'f35a359c06d8c2d6a60267187bc93e6e96a019574ec260ca2206b8e43e5109d9', '1', 0);
+(21, 1, '2022-06-21 10:26:56', 'Reset Password Admin Account:ituser - DETAILS : \r\n({\"ADMIN_ID\":\"1\",\"ADMIN_NO\":\"ituser\",\"FIRSTNAME\":\"aaaa\",\"LASTNAME\":\"ddd\",\"USERNAME\":\"ituser\",\"USER_ROLE\":\"1\"})', 'f35a359c06d8c2d6a60267187bc93e6e96a019574ec260ca2206b8e43e5109d9', '1', 0),
+(22, 1, '2022-06-26 22:06:41', 'Add User depart Details::{\"USER_ID\":9,\"USERNAME\":\"depart\",\"FIRST_NAME\":\"depter\",\"LAST_NAME\":\"partment\",\"USER_ROLE\":\"3\",\"EMAIL\":\"depter@gmail.com\"}', '812cb7465ec7816e194fe3963b2a234d4fb4dcbe079ae3ce0eb3cb83323d3f22', '1', 0),
+(23, 1, '2022-06-26 22:07:00', 'Reset Password Admin Account:depart - DETAILS : \r\n({\"ADMIN_ID\":\"9\",\"ADMIN_NO\":\"depart\",\"FIRSTNAME\":\"depter\",\"LASTNAME\":\"partment\",\"USERNAME\":\"depart\",\"USER_ROLE\":\"3\"})', '812cb7465ec7816e194fe3963b2a234d4fb4dcbe079ae3ce0eb3cb83323d3f22', '1', 0);
 
 -- --------------------------------------------------------
 
@@ -930,7 +932,8 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`user_id`, `id_no`, `location`, `firstname`, `middlename`, `lastname`, `user_role`, `username`, `password`, `email_address`, `status`, `locked`, `last_signin`) VALUES
 (1, 'ituser', 'upload\\user_image_62b5dd1ccd7231.tmp', 'Ronnel', '', 'Macompas', 1, 'ituser', '8bdcd09f5a8f9c55cb6702aed6728362db813e97', 'ronnel@yahoo.com', 0, 0, NULL),
 (2, '', '', 'SAMPLE_REGS', '', 'LAST', 2, '', 'a195c5c1019326441def43766894f74373e13003', '', 1, 0, NULL),
-(8, 'nel', '', 'neru', '', 'tokisaki', 2, 'nel', '5ecab09f348cc97279a616079664d6373c1b4ec6', 'neru@gmail.com', 0, 0, NULL);
+(8, 'nel', '', 'neru', '', 'tokisaki', 2, 'nel', '5ecab09f348cc97279a616079664d6373c1b4ec6', 'neru@gmail.com', 0, 0, NULL),
+(9, 'depart', '', 'depter', '', 'partment', 3, 'depart', '8d511fcfad2580e41f55e9fb06cf592e9af8a458', 'depter@gmail.com', 0, 0, NULL);
 
 -- --------------------------------------------------------
 
@@ -1025,7 +1028,8 @@ INSERT INTO `user_log` (`user_log_id`, `login_date`, `logout_date`, `action`, `u
 (68, '2022-06-24 23:44:46', '0000-00-00 00:00:00', 'LOGIN', 1, '307a2296737d52960c1366a00f89240e8349cf7ed9cdbfbcdd5cc5b080e2c151', '::1', '{\"device\":\"Microsoft Edge\",\"version\":\"102.0.1245.44\",\"layout\":\"Blink\",\"os\":{\"architecture\":64,\"family\":\"Windows\",\"version\":\"10\"},\"description\":\"Microsoft Edge 102.0.1245.44 on Windows 10 64-bit\"}', 0),
 (69, '2022-06-25 11:16:16', '2022-06-25 14:12:05', 'LOGIN', 1, '7899d4a2619468b0569246e4b87d5e835193f0db953e916089508db36222188e', '::1', '{\"device\":\"Microsoft Edge\",\"version\":\"103.0.1264.37\",\"layout\":\"Blink\",\"os\":{\"architecture\":64,\"family\":\"Windows\",\"version\":\"10\"},\"description\":\"Microsoft Edge 103.0.1264.37 on Windows 10 64-bit\"}', 0),
 (70, '2022-06-25 14:12:23', '0000-00-00 00:00:00', 'LOGIN', 1, 'eff7724750620434b35be34537bb9d76272ebfb4db2d29bde290b8e177fedc55', '::1', '{\"device\":\"Microsoft Edge\",\"version\":\"103.0.1264.37\",\"layout\":\"Blink\",\"os\":{\"architecture\":64,\"family\":\"Windows\",\"version\":\"10\"},\"description\":\"Microsoft Edge 103.0.1264.37 on Windows 10 64-bit\"}', 0),
-(71, '2022-06-26 15:32:15', '0000-00-00 00:00:00', 'LOGIN', 1, '812cb7465ec7816e194fe3963b2a234d4fb4dcbe079ae3ce0eb3cb83323d3f22', '::1', '{\"device\":\"Microsoft Edge\",\"version\":\"103.0.1264.37\",\"layout\":\"Blink\",\"os\":{\"architecture\":64,\"family\":\"Windows\",\"version\":\"10\"},\"description\":\"Microsoft Edge 103.0.1264.37 on Windows 10 64-bit\"}', 0);
+(71, '2022-06-26 15:32:15', '0000-00-00 00:00:00', 'LOGIN', 1, '812cb7465ec7816e194fe3963b2a234d4fb4dcbe079ae3ce0eb3cb83323d3f22', '::1', '{\"device\":\"Microsoft Edge\",\"version\":\"103.0.1264.37\",\"layout\":\"Blink\",\"os\":{\"architecture\":64,\"family\":\"Windows\",\"version\":\"10\"},\"description\":\"Microsoft Edge 103.0.1264.37 on Windows 10 64-bit\"}', 0),
+(72, '2022-06-26 22:08:11', '0000-00-00 00:00:00', 'LOGIN', 9, '81e06fb019ea6c5c6618d71366ea166873b311437fbf022be3be7f5166431ec4', '::1', '{\"device\":\"Microsoft Edge\",\"version\":\"103.0.1264.37\",\"layout\":\"Blink\",\"os\":{\"architecture\":64,\"family\":\"Windows\",\"version\":\"10\"},\"description\":\"Microsoft Edge 103.0.1264.37 on Windows 10 64-bit\"}', 0);
 
 --
 -- Indexes for dumped tables
@@ -1093,7 +1097,7 @@ ALTER TABLE `user_log`
 -- AUTO_INCREMENT for table `activity_log`
 --
 ALTER TABLE `activity_log`
-  MODIFY `activity_log_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `activity_log_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `program`
@@ -1135,13 +1139,13 @@ ALTER TABLE `teacher_class`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `user_log`
 --
 ALTER TABLE `user_log`
-  MODIFY `user_log_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=72;
+  MODIFY `user_log_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=73;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
